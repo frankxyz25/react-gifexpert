05c713d501b76cacbf537e00e2800728b0233bcc
Franklin Harding <franklinharding108@gmail.com>
2020-10-06 21:39:13 -0500

    second commit

---
+++ docs/precache-manifest.57231b1301a18c692ed5ed33a7287282.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "427e67cdafde20d072632a58cc010016",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4d4a33da291622941955",
+    "url": "/static/css/main.0e7e05cf.chunk.css"
+  },
+  {
+    "revision": "51858a585169eeb49f95",
+    "url": "/static/js/2.56c56de3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56c56de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d4a33da291622941955",
+    "url": "/static/js/main.8791cc9a.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
